6f7e72035da0ef2554167c2774397856c628ab58
Moustafa Mohsen <moustafamohsen1@gmail.com>
2018-07-27 15:34:59 +0200

    stable version before translation

--- src/app/topbar/topbar.component.spec.ts
+++ src/app/topbar/topbar.component.spec.ts
@@ -23,3 +23,116 @@ describe('TopbarComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+//OLD CODE
+/*
+//Find hadith by number
+  CalcHadithNumber(hadithChapter?:HadithBook[],hadithnumber?:number){//(click) test
+    var hadithcount:number=0;
+    //loop to count
+    hadithChapter.forEach(
+      book => {
+        book.hadiths.forEach(
+          hadith => {
+            hadithcount++;
+            if (hadithcount==hadithnumber) {
+              //execute logic for hadith numbers
+              console.log("count==");
+              console.log(hadithcount);
+              console.log("==count");
+              console.log(book.book+" book");
+              console.log(hadith.hadith+" ");
+            }
+        });
+    });
+    console.log(hadithcount);
+  }
+
+//Loop script
+Hadithbooks:HadithBook[]=[{book:0,hadiths:null},{book:0,hadiths:null}];
+hadiths:hadith[];
+  //Loop Script
+  loop(){
+    for (let i = 1; i <= 93; i++) {
+      this.http.get<hadith[]>("https://muflihun.com/svc/hadith?c=1&b="+i).subscribe(
+        r=>{
+          var hold :HadithBook={book:0,hadiths:null}
+          hold.book=i
+          hold.hadiths=r
+          this.Hadithbooks.push(hold)
+
+        });
+     // books.push(hold)
+    }
+  }//loop
+  printloop(){
+    console.log(this.Hadithbooks);
+    
+  }
+
+    //Chapter Select Toggle
+  toggleChapterSelect(){
+    //check if select Chapter is disabled
+    if (this.chapter==""||this.chapter==null) {
+      this.disableChapterSelect=false
+    }
+    else{this.disableChapterSelect=true}
+    //update Hadith Count
+    //By chapter option
+    if(this.chapter_book!=null){
+     // this.hadithinChapterCount=this.GetHadithsCountinBook(this.chapter_book,BukhariLog)
+    }
+    //By chapter number
+      else{
+       // this.hadithinChapterCount=this.GetHadithsCountinBook(this.chapter,BukhariLog)
+      }
+      
+
+    
+  }
+
+    //=================GETTING VALUES//
+
+  //muslim https://muflihun.com/svc/hadith?c=2&b=1&h=1
+  Hadithbooks:HadithBook[]=[{book:0,hadiths:null},{book:0,hadiths:null}];
+  hadiths:hadith[];
+
+    //Loop Script
+    loop(){
+      this.loopget(43)
+    }//loop
+
+    loopget(i){
+      //to get log type from the internet
+      
+      if(i==0) {return}
+      this.http.get<hadith[]>("https://muflihun.com/svc/hadith?c=2&b="+i).subscribe(
+        r=>{
+          var hold :HadithBook={book:0,hadiths:null}
+          hold.book=i
+          hold.hadiths=r
+          this.Hadithbooks.push(hold)
+          this.loopget(i-1);
+        });
+      }
+
+      xx:hadithaddress[]=[];
+      loop2(){
+        //to convert Log to adress
+        
+        let x:hadithaddress;
+        for (let i = 0; i <= 43; i++) {
+          
+          x= this.GetHadithsFromTo(i,MuslimLog,MuslimEnglishIndex);
+          console.log(x);
+          this.xx.push(x);
+        }
+        console.log(this.xx);
+        
+      }
+      printloop(){
+        console.log(this.Hadithbooks);
+        
+      }
+      //GETTING VALUES=================//
+
+ */
--- src/app/topbar/topbar.component.ts
+++ src/app/topbar/topbar.component.ts
@@ -52,6 +52,7 @@ export class TopbarComponent implements OnInit {
   rFQ: FormGroup;
   source_options:FormControl=new FormControl('Quran',Validators.required);
 
+
   //=======================================================================================ngOnInit//
   ngOnInit() {
 
@@ -99,6 +100,9 @@ export class TopbarComponent implements OnInit {
       
     });
 
+
+    //=======Value Changes
+
     this.rFH.valueChanges.subscribe(
       form=>{
         //this.NextValid();
@@ -143,8 +147,6 @@ export class TopbarComponent implements OnInit {
         } else {
           return
         }
-
-
       }
     )
 
@@ -173,8 +175,11 @@ export class TopbarComponent implements OnInit {
     this.rFQ.get('surah_number').valueChanges.subscribe(
       value => {
         this.SetMaxAyat();
+        this.rFQ.get('ayat_number').setValue(1)
+        this.rFQ.get('ayat_number').setValidators([Validators.required,Validators.max(this.ayatMax),Validators.min(1)])
       }
     )
+    //Value Changes=======
 
   }//ngOnInit========================================================================================//
 
@@ -293,10 +298,19 @@ export class TopbarComponent implements OnInit {
 
 
   SetMaxAyat() {
-    let value = this.rFQ.get('surah_number').value;
-    if (value <= 0) return
-    let Surrah = QuranIndex.filter(f => f.number == value);
+    let suran = this.rFQ.get('surah_number').value;
+    if (suran <= 0||suran > 114) return
+    let Surrah = QuranIndex.filter(f => f.number == suran);
     this.ayatMax = Surrah[0].numberOfAyahs;
+    console.log('====maxAyat');
+
+    console.log(this.ayatMax);
+    console.log( this.rFQ.get('surah_number').getError('max') );
+    
+    console.log('maxAyat====');
+    this.getvalidations(this.rFQ)
+    
+    
   }
 
 
@@ -338,12 +352,12 @@ export class TopbarComponent implements OnInit {
 
 
   //===========Testing==========//
-  getvalidations(rFH:FormGroup){
+  getvalidations(rF:FormGroup){
     console.log('====Validation');
     
-    Object.keys(rFH.controls).forEach(key => {
+    Object.keys(rF.controls).forEach(key => {
 
-      let controlErrors: ValidationErrors = rFH.get(key).errors;
+      let controlErrors: ValidationErrors = rF.get(key).errors;
       if (controlErrors != null) {
             Object.keys(controlErrors).forEach(keyError => {
               console.log('Key control: "' + key + '", keyError: "' + keyError + '", err value: ', controlErrors[keyError]);
@@ -355,119 +369,3 @@ export class TopbarComponent implements OnInit {
   }
 
 } //CLASS
-
-
-
-//OLD CODE
-/*
-//Find hadith by number
-  CalcHadithNumber(hadithChapter?:HadithBook[],hadithnumber?:number){//(click) test
-    var hadithcount:number=0;
-    //loop to count
-    hadithChapter.forEach(
-      book => {
-        book.hadiths.forEach(
-          hadith => {
-            hadithcount++;
-            if (hadithcount==hadithnumber) {
-              //execute logic for hadith numbers
-              console.log("count==");
-              console.log(hadithcount);
-              console.log("==count");
-              console.log(book.book+" book");
-              console.log(hadith.hadith+" ");
-            }
-        });
-    });
-    console.log(hadithcount);
-  }
-
-//Loop script
-Hadithbooks:HadithBook[]=[{book:0,hadiths:null},{book:0,hadiths:null}];
-hadiths:hadith[];
-  //Loop Script
-  loop(){
-    for (let i = 1; i <= 93; i++) {
-      this.http.get<hadith[]>("https://muflihun.com/svc/hadith?c=1&b="+i).subscribe(
-        r=>{
-          var hold :HadithBook={book:0,hadiths:null}
-          hold.book=i
-          hold.hadiths=r
-          this.Hadithbooks.push(hold)
-
-        });
-     // books.push(hold)
-    }
-  }//loop
-  printloop(){
-    console.log(this.Hadithbooks);
-    
-  }
-
-    //Chapter Select Toggle
-  toggleChapterSelect(){
-    //check if select Chapter is disabled
-    if (this.chapter==""||this.chapter==null) {
-      this.disableChapterSelect=false
-    }
-    else{this.disableChapterSelect=true}
-    //update Hadith Count
-    //By chapter option
-    if(this.chapter_book!=null){
-     // this.hadithinChapterCount=this.GetHadithsCountinBook(this.chapter_book,BukhariLog)
-    }
-    //By chapter number
-      else{
-       // this.hadithinChapterCount=this.GetHadithsCountinBook(this.chapter,BukhariLog)
-      }
-      
-
-    
-  }
-
-    //=================GETTING VALUES//
-
-  //muslim https://muflihun.com/svc/hadith?c=2&b=1&h=1
-  Hadithbooks:HadithBook[]=[{book:0,hadiths:null},{book:0,hadiths:null}];
-  hadiths:hadith[];
-
-    //Loop Script
-    loop(){
-      this.loopget(43)
-    }//loop
-
-    loopget(i){
-      //to get log type from the internet
-      
-      if(i==0) {return}
-      this.http.get<hadith[]>("https://muflihun.com/svc/hadith?c=2&b="+i).subscribe(
-        r=>{
-          var hold :HadithBook={book:0,hadiths:null}
-          hold.book=i
-          hold.hadiths=r
-          this.Hadithbooks.push(hold)
-          this.loopget(i-1);
-        });
-      }
-
-      xx:hadithaddress[]=[];
-      loop2(){
-        //to convert Log to adress
-        
-        let x:hadithaddress;
-        for (let i = 0; i <= 43; i++) {
-          
-          x= this.GetHadithsFromTo(i,MuslimLog,MuslimEnglishIndex);
-          console.log(x);
-          this.xx.push(x);
-        }
-        console.log(this.xx);
-        
-      }
-      printloop(){
-        console.log(this.Hadithbooks);
-        
-      }
-      //GETTING VALUES=================//
-
- */
